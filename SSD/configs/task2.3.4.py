adf409a4c3c6feb0dcb302eb7a47f9219901bf7d
Gaute Wierød Rønning <gautewr@clab06.idi.ntnu.no>
2022-05-05 18:31:30 +0200

    Config files for 2.3

--- SSD/configs/task2.3.4.py
+++ SSD/configs/task2.3.4.py
@@ -5,7 +5,7 @@ from tops.config import LazyCall as L
 from ssd.data.transforms import (
     ToTensor, Normalize, Resize, RandomSampleCrop, RandomHorizontalFlip,
     GroundTruthBoxesToAnchors)
-from .ssd128Retina import train, anchors, optimizer, schedulers, backbone, model, data_train, data_val, loss_objective
+from .ssd128RetinaFocalImp import train, anchors, optimizer, schedulers, backbone, model, data_train, data_val, loss_objective
 from .utils import get_dataset_dir
 
 # Keep the model, except change the backbone and number of classes
--- SSD/ssd/modeling/ssd.py
+++ SSD/ssd/modeling/ssd.py
@@ -31,6 +31,108 @@ class SSD300(nn.Module):
         self.regression_heads = nn.ModuleList(self.regression_heads)
         self.classification_heads = nn.ModuleList(self.classification_heads)
         self.anchor_encoder = AnchorEncoder(anchors)
+        self._init_weights()
+
+    def _init_weights(self):
+        layers = [*self.regression_heads, *self.classification_heads]
+        for layer in layers:
+            for param in layer.parameters():
+                if param.dim() > 1: nn.init.xavier_uniform_(param)
+
+    def _init_weights_improved(self):
+        
+        for layer in self.regression_heads:
+            layer.bias.data.fill_(0)
+            for param in layer.parameters():
+                if param.dim() > 1:
+                    nn.init.xavier_uniform_(param)
+        
+
+        for layer in self.classification_heads:
+            layer.bias.data.fill_(0)
+            nn.init.normal_(layer.weight, 0, 0.01)
+
+        #pi is average amount of anchor boxes that will hit foreground
+        #pi = 0.01
+        #-torch.log((1-pi)/pi
+        #p is the opposite of pi pretty much, amount of background. k is just numclasses-1
+        #0 0 0 0 0 0  1 1 1 1 1 format på data
+        #configs/task2.3.3.py
+        p = 0.995
+        print("WEEEEEEE", self.classification_heads[-1].bias.data.shape)
+        self.classification_heads[-1].bias.data[:len(self.classification_heads[-1].bias)//9], np.log(p*(self.num_classes - 1)/(1-p))
+
+    def regress_boxes(self, features):
+        locations = []
+        confidences = []
+        for idx, x in enumerate(features):
+            bbox_delta = self.regression_heads[idx](x).view(x.shape[0], 4, -1)
+            bbox_conf = self.classification_heads[idx](x).view(x.shape[0], self.num_classes, -1)
+            locations.append(bbox_delta)
+            confidences.append(bbox_conf)
+        bbox_delta = torch.cat(locations, 2).contiguous()
+        confidences = torch.cat(confidences, 2).contiguous()
+        return bbox_delta, confidences
+
+    
+    def forward(self, img: torch.Tensor, **kwargs):
+        """
+            img: shape: NCHW
+        """
+        if not self.training:
+            return self.forward_test(img, **kwargs)
+        features = self.feature_extractor(img)
+        return self.regress_boxes(features)
+    
+    def forward_test(self,
+            img: torch.Tensor,
+            imshape=None,
+            nms_iou_threshold=0.5, max_output=200, score_threshold=0.05):
+        """
+            img: shape: NCHW
+            nms_iou_threshold, max_output is only used for inference/evaluation, not for training
+        """
+        features = self.feature_extractor(img)
+        bbox_delta, confs = self.regress_boxes(features)
+        boxes_ltrb, confs = self.anchor_encoder.decode_output(bbox_delta, confs)
+        predictions = []
+        for img_idx in range(boxes_ltrb.shape[0]):
+            boxes, categories, scores = filter_predictions(
+                boxes_ltrb[img_idx], confs[img_idx],
+                nms_iou_threshold, max_output, score_threshold)
+            if imshape is not None:
+                H, W = imshape
+                boxes[:, [0, 2]] *= H
+                boxes[:, [1, 3]] *= W
+            predictions.append((boxes, categories, scores))
+        return predictions
+
+class SSD300ImpW(nn.Module):
+    def __init__(self, 
+            feature_extractor: nn.Module,
+            anchors,
+            loss_objective,
+            num_classes: int):
+        super().__init__()
+        """
+            Implements the SSD network.
+            Backbone outputs a list of features, which are gressed to SSD output with regression/classification heads.
+        """
+
+        self.feature_extractor = feature_extractor
+        self.loss_func = loss_objective
+        self.num_classes = num_classes
+        self.regression_heads = []
+        self.classification_heads = []
+
+        # Initialize output heads that are applied to each feature map from the backbone.
+        for n_boxes, out_ch in zip(anchors.num_boxes_per_fmap, self.feature_extractor.out_channels):
+            self.regression_heads.append(nn.Conv2d(out_ch, n_boxes * 4, kernel_size=3, padding=1))
+            self.classification_heads.append(nn.Conv2d(out_ch, n_boxes * self.num_classes, kernel_size=3, padding=1))
+
+        self.regression_heads = nn.ModuleList(self.regression_heads)
+        self.classification_heads = nn.ModuleList(self.classification_heads)
+        self.anchor_encoder = AnchorEncoder(anchors)
         self._init_weights_improved()
 
     def _init_weights(self):
