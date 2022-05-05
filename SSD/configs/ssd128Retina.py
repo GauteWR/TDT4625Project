8ec476e48d26b93f6e66b4639c2999c7b82a23ab
Gaute Wierød Rønning <gautewr@clab06.idi.ntnu.no>
2022-05-05 12:36:49 +0200

    2.3.1

--- SSD/configs/ssd128Retina.py
+++ SSD/configs/ssd128Retina.py
@@ -29,7 +29,6 @@ anchors = L(AnchorBoxes)(
     # Strides is the number of pixels (in image space) between each spatial position in the feature map
     strides=[[4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]],
     min_sizes=[[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
-    # aspect ratio is defined per feature map (first index is largest feature map (38x38))
     # aspect ratio is used to define two boxes per element in the list.
     # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
     # Number of boxes per location is in total 2 + 2 per aspect ratio
@@ -40,7 +39,7 @@ anchors = L(AnchorBoxes)(
 )
 
 backbone = L(pyramid.PyramidModel)(
-    output_channels=[512, 1024, 512, 512, 128, 128],
+    output_channels=[512, 1024, 512, 512, 128, 1024],
     image_channels="${train.image_channels}",
     output_feature_sizes="${anchors.feature_sizes}"
 )
--- SSD/ssd/modeling/backbones/pyramid.py
+++ SSD/ssd/modeling/backbones/pyramid.py
@@ -14,77 +14,83 @@ class PyramidModel(torch.nn.Module):
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
 
+        self.m = torchvision.ops.FeaturePyramidNetwork([1024, 512, 512, 128, 128], 1024)
+
         # Implement RetinaNet
 
         self.resnet = models.resnet18(pretrained=True)
 
         self.additional_layers = torch.nn.ModuleList([
-            torch.nn.Sequential( # 19 x 19 out
+            torch.nn.Sequential( 
                 torch.nn.Conv2d(output_channels[0], 2048, kernel_size=3, padding=1),
                 torch.nn.ReLU(),
                 torch.nn.Conv2d(2048, output_channels[1], kernel_size=3, padding=1, stride=2),
                 torch.nn.ReLU(),
             ),
-            torch.nn.Sequential( # 10x10 out
+            torch.nn.Sequential( 
                 torch.nn.Conv2d(output_channels[1], 512, kernel_size=3, padding=1),
                 torch.nn.ReLU(),
                 torch.nn.Conv2d(512, output_channels[2], kernel_size=3, padding=1, stride=2),
                 torch.nn.ReLU(),
             ),
-            torch.nn.Sequential( # 5 x 5 out
+            torch.nn.Sequential(
                 torch.nn.Conv2d(output_channels[2], 512, kernel_size=3, padding=1),
                 torch.nn.ReLU(),
                 torch.nn.Conv2d(512, output_channels[3], kernel_size=3, padding=1, stride=2),
                 torch.nn.ReLU(),
             ),
-            torch.nn.Sequential( # 3 x 3 out
+            torch.nn.Sequential( 
                 torch.nn.Conv2d(output_channels[3], 128, kernel_size=3, padding=1),
                 torch.nn.ReLU(),
                 torch.nn.Conv2d(128, output_channels[4], kernel_size=3, stride=2, padding=1),
                 torch.nn.ReLU(),
             ),
-            torch.nn.Sequential( # 1 x 1 out
-                torch.nn.Conv2d(output_channels[4], 128, kernel_size=2, padding=1),
-                torch.nn.ReLU(),
-                torch.nn.Conv2d(128, output_channels[5], kernel_size=2, padding=1),
-                torch.nn.ReLU(),
-                torch.nn.MaxPool2d(2,2)
-            ),
         ])
 
     def forward(self, x):
+        #print(x.shape)
         x = self.resnet.conv1(x)
         x = self.resnet.bn1(x)
         x = self.resnet.relu(x)
         x = self.resnet.maxpool(x)
         x1 = x
+        #print(x1.shape)
         for layer in self.resnet.layer1:
             x1 = layer(x1)
         x2 = x1
+        #print(x2.shape)
         for layer in self.resnet.layer2:
             x2 = layer(x2)
         x3 = x2
+        #print(x3.shape)
         for layer in self.resnet.layer3:
             x3 = layer(x3)
         x4 = x3
+        #print(x4.shape)
         for layer in self.resnet.layer4:
             x4 = layer(x4)
         x5 = x4
-        print("Done with resnet")
-        out_features = [x, x1, x2, x3, x4]
+        #print(x5.shape)
+        out_features = [x4]
         for i, layer in enumerate(self.additional_layers.children()):
             x5 = layer(x5)
-            if i == len(self.additional_layers)-1:
-                out_features.append(x5)
-            print(x5.shape)
-        print(len(out_features))
-        
-        self.m = torchvision.ops.FeaturePyramidNetwork(dict(out_features), [64, 128, 256, 512, 1024])
-        
+            # if i == len(self.additional_layers)-1:
+            out_features.append(x5)
+            # print(x5.shape)
+        # print(len(out_features))
+        # out_feat = dict(out_features)
+        m_dict = {
+            "tensor1": out_features[len(out_features)-1],
+        }
+        x6 = self.m.forward(m_dict)
+        x6 = x6['tensor1'].to('cuda')
+        out_features.append(x6)
+        # print(x6['tensor1'].shape)
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
             expected_shape = (out_channel, h, w)
+            # print(feature.shape, "vs", expected_shape, " at idx:", idx)
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
         assert len(out_features) == len(self.output_feature_shape),\
